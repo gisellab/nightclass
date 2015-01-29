3d7b42612cff8ba5f99dbf05ec8c174655591e1c
gisella barbaray <gisellabarbaray@gmail.com>
2015-01-28 18:39:36 -0800

    init commit

---
+++ new.py
@@ -0,0 +1,22 @@
+newdict = {'key': 'value'}
+
+print newdict
+print newdict ['key']
+
+list = ['apple', 'orange', 40, 'grapes', 'last item']
+Dictionary = {'apple', 'orange', 40, 'grapes', 'last item'}
+for items in list :
+	print items
+
+print len (list)
+print list {ind-1}
+
+num = 1
+
+while num > 0:
+	num += 1
+	print num
+
+Dictionary = {'blue', 'red', 'green'}
+for items in list :
+print Dictionary
